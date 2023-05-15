ff28550cf60cacdfcfe142e1375872cbc0be73dd
ezequiel-g <ezequielghione@gmail.com>
2023-05-15 19:09:25 -0300

    round[2] - REFACTOR: rename parameter

--- src/main/java/poc/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/poc/tdd/fizzbuzz/FizzBuzz.java
@@ -1,8 +1,8 @@
 package poc.tdd.fizzbuzz;
 
 public class FizzBuzz {
-    public int get(final int i) {
-        return i;
+    public int get(final int position) {
+        return position;
     }
 
 }
