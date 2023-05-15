cfc76bad6a4c131bf445ae83f370d7d26f1e0d4d
ezequiel-g <ezequielghione@gmail.com>
2023-05-15 19:26:31 -0300

    round[3] - REFACTOR TEST: use junit parametrized feature

--- src/test/java/poc/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/poc/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,6 +1,11 @@
 package poc.tdd.fizzbuzz;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -8,7 +13,16 @@ public class FizzBuzzTest {
 
     private final FizzBuzz fizzBuzz = new FizzBuzz();
 
-    private void testParametrized(final int input, final String expected) {
+    private static Stream<Arguments> arguments() {
+        return Stream.of(
+                Arguments.of(2, "2"),
+                Arguments.of(1, "1")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("arguments")
+    void testParametrized(final int input, final String expected) {
         assertEquals(expected, fizzBuzz.get(input));
     }
 
