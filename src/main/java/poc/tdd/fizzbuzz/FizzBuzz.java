f23db8a8bcacded4d761f9a00dbdda3fca57823f
ezequiel-g <ezequielghione@gmail.com>
2023-05-15 19:31:47 -0300

    round[4] - REFACTOR: extract constant for fizz

--- src/main/java/poc/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/poc/tdd/fizzbuzz/FizzBuzz.java
@@ -1,9 +1,12 @@
 package poc.tdd.fizzbuzz;
 
 public class FizzBuzz {
+
+    public static final String FIZZ = "fizz";
+
     public String get(final int position) {
         if (position % 3 == 0) {
-            return "fizz";
+            return FIZZ;
         }
 
         return Integer.toString(position);
