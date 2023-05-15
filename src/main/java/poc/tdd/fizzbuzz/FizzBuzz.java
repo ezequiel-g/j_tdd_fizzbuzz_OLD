f6ac1642739a6d5c47cd45df7a68aa2f2f508ec5
ezequiel-g <ezequielghione@gmail.com>
2023-05-15 19:30:37 -0300

    round[4] - FIX: include the simplest code change that fix the test

--- src/main/java/poc/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/poc/tdd/fizzbuzz/FizzBuzz.java
@@ -2,6 +2,10 @@ package poc.tdd.fizzbuzz;
 
 public class FizzBuzz {
     public String get(final int position) {
+        if (position % 3 == 0) {
+            return "fizz";
+        }
+
         return Integer.toString(position);
     }
 
