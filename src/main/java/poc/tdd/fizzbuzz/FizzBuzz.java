ccb6d75659397cc6b0d2214a0c5cd13b14cda2e2
ezequiel-g <ezequielghione@gmail.com>
2023-05-15 22:57:53 -0300

    round[6] - REFACTOR: extract constants for numeric values

--- src/main/java/poc/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/poc/tdd/fizzbuzz/FizzBuzz.java
@@ -4,11 +4,13 @@ public class FizzBuzz {
 
     public static final String FIZZ = "fizz";
     public static final String BUZZ = "buzz";
+    public static final int FIZZ_VALUE = 3;
+    public static final int BUZZ_VALUE = 5;
 
     public String get(final int position) {
 
-        final boolean isFizz = position % 3 == 0;
-        final boolean isBuzz = position % 5 == 0;
+        final boolean isFizz = position % FIZZ_VALUE == 0;
+        final boolean isBuzz = position % BUZZ_VALUE == 0;
 
         if (isFizz && isBuzz) {
             return FIZZ + BUZZ;
